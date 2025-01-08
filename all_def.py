5aaeaf159a684fa1595d8c955fed78970e5546ea
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2025-01-08 12:04:21 -0500

    Create all_def.py

---
+++ all_def.py
@@ -0,0 +1,87 @@
+def even_odd():                   # Short example
+    n = input("enter number:")
+    n = int(n)
+    if n % 2 ==0:
+        print("n is even")
+    else:
+        print("n is odd")
+
+even_odd()
+even_odd()
+even_odd()
+
+n = input("enter you number:")     # Long example
+n = int(n)
+
+if n % 2 == 0:
+    print("n - even")
+else:
+    print("n - odd")
+
+n = input("enter you number:")
+n = int(n)
+
+if n % 2 == 0:
+    print("n - even")
+else:
+    print("n - odd")
+
+n = input("enter you number:")
+n = int(n)
+
+if n % 2 == 0:
+    print("n - even")
+else:
+    print("n - odd")
+
+# A lot more :
+def f(x):
+    return x+1
+
+z = f(4)
+
+if z == 5:
+    print("z equal 5")
+else:
+    print("z is not equal 5")
+
+
+def f():
+    return 1+1
+
+result = f()
+print(result)
+
+
+def f(x, y, z):
+    return x+y+z
+
+result = f(4,5,1)
+print(result)
+
+def f():
+    z= 1+1
+result = f()
+print(result)
+
+len('USA')
+
+# all build in functions
+
+age = input("Give your age:")
+int_age = int(age)
+if int_age < 21:
+    print("You are awesome !")
+else:
+    print("You are old dude!")
+
+# Many times def
+
+def even_odd(x):
+    if x % 2 ==0:
+        print("even")
+    else:
+        print("odd")
+
+result = even_odd(4)
+print(result)
