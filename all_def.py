c133044554faba0e9e7d75c2979ce0f6ec6214f0
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2025-01-12 22:54:31 -0500

    Update all_def.py

--- all_def.py
+++ all_def.py
@@ -94,3 +94,21 @@ def add_it(x, y=10):
     return x+y
 result = add_it(2)
 print(result)
+
+
+
+import math
+
+#long of diaoganal 
+
+l=4
+w=10
+d= math.sqrt(l**2 + w**2)
+
+print(d)
+
+
+a = 'qwertyuiopasdfghjklzxcvbnm'
+
+print(*sorted(a))
+
